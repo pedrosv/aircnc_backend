c51f8ac4b76fcafbc3baa3f1d3598286345ec8b2
pedrosv <pedrosv6@gmail.com>
2019-10-08 01:56:50 -0300

    Backend depois da primeira aula

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routs');
+
+const app = express();
+mongoose.connect('mongodb+srv://pedrosv:omnistack@omni9-iqegd.mongodb.net/semana9?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+//req.query = acessar query params (para filtros) /users/2
+//req.params = acessar route params (edicao e delete) /users?id=2
+//req.body = Acessar corpo da requisicao  json
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
