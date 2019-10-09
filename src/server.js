169c938f1a0ca0b91ba0aae9cc5e80268f8c4654
pedrosv <pedrosv6@gmail.com>
2019-10-09 03:28:40 -0300

    enviando link da imagem

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routs');
+const path = require('path');
 
 
 const app = express();
@@ -15,6 +16,7 @@ mongoose.connect('mongodb+srv://pedrosv:omnistack@omni9-iqegd.mongodb.net/semana
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
